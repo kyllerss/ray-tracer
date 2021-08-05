6ea6cf3682c8cea01b793da2df2d925dcbb35ce7
Kyle L <klomeli@gmail.com>
2021-08-05 15:48:12 -0400

    Added equality operator overloading.

--- src/domain/mod.rs
+++ src/domain/mod.rs
@@ -9,8 +9,24 @@ struct RayTuple {
 }
 
 impl RayTuple {
+    const EPSILON: f64 = 0.00001;
+
     fn new(x: f64, y: f64, z: f64, w: f64) -> RayTuple {
         RayTuple{x, y, z, w}
+    }
+
+    fn epsilon_eq(a: f64, b: f64) -> bool {
+        f64::abs(a - b) < RayTuple::EPSILON
+    }
+}
+
+impl PartialEq for RayTuple {
+
+    fn eq(&self, other: &Self) -> bool {
+        RayTuple::epsilon_eq(self.x, other.x)
+            && RayTuple::epsilon_eq(self.y, other.y)
+            && RayTuple::epsilon_eq(self.z, other.z)
+            && RayTuple::epsilon_eq(self.w, other.w)
     }
 }
 
@@ -38,6 +54,12 @@ impl Point {
     }
 }
 
+impl PartialEq for Point {
+    fn eq(&self, other: &Self) -> bool {
+        self.ray_tuple == other.ray_tuple
+    }
+}
+
 impl ToRayTuple for Point {
     fn to_ray_tuple(&self) -> (f64, f64, f64, f64) {
         (self.ray_tuple.x,
@@ -53,6 +75,12 @@ impl Vector {
     // constructor
     pub fn new(x: f64, y: f64, z: f64) -> Vector {
         Vector { ray_tuple: RayTuple::new(x, y, z, Vector::W) }
+    }
+}
+
+impl PartialEq for Vector {
+    fn eq(&self, other: &Self) -> bool {
+        self.ray_tuple == other.ray_tuple
     }
 }
 
--- src/domain/tests.rs
+++ src/domain/tests.rs
@@ -11,6 +11,15 @@ mod tests {
         let _z = 3.3;
         let p = Point::new(_x, _y, _z);
         assert_eq!(p.to_ray_tuple(), (_x, _y, _z, 1.0));
+
+        // test equality
+        let a = Point::new(1.0000001, 2.00000002, 3.000000003);
+        let b = Point::new(1.000000001, 2.00000002, 3.00000003);
+        assert_eq!(a, b);
+
+        let c = Point::new(1.01, 2.02, 3.03);
+        let d = Point::new(1.0, 2.0, 3.0);
+        assert_ne!(c, d);
     }
 
     #[test]
@@ -21,6 +30,15 @@ mod tests {
         let _z = 3.3;
         let p = Vector::new(_x, _y, _z);
         assert_eq!(p.to_ray_tuple(), (_x, _y, _z, 0.0));
+
+        // test equality
+        let a = Vector::new(1.0000001, 2.00000002, 3.000000003);
+        let b = Vector::new(1.000000001, 2.00000002, 3.00000003);
+        assert_eq!(a, b);
+
+        let c = Vector::new(1.01, 2.02, 3.03);
+        let d = Vector::new(1.0, 2.0, 3.0);
+        assert_ne!(c, d);
     }
 
 }
