94477e060d7af51e0eec6668e965fd724bd72379
Kyle L <klomeli@gmail.com>
2021-08-16 14:12:50 -0400

    Added matrix by matrix multiplication.

--- src/domain/matrix.rs
+++ src/domain/matrix.rs
@@ -1,4 +1,4 @@
-use std::ops::{Index, IndexMut};
+use std::ops::{Index, IndexMut, Mul};
 
 #[derive(Clone, Debug, PartialEq)]
 pub struct Matrix {
@@ -38,5 +38,20 @@ impl IndexMut<usize> for Matrix {
         let p = &mut self.contents;
         let r: &mut [f64] = &mut p[start..start + self.width];
         r
+    }
+}
+
+fn m(row: usize, col: usize, a: &Matrix, b: &Matrix) -> f64 {
+    a[row][0] * b[0][col] + a[row][1] * b[1][col] + a[row][2] * b[2][col] + a[row][3] * b[3][col]
+}
+
+impl<'a> Mul<&'a Matrix> for Matrix {
+    type Output = Matrix;
+    fn mul(self, rhs: &'a Matrix) -> Self::Output {
+        use crate::matrix;
+        matrix![m(0,0,&self,rhs), m(0,1,&self,rhs), m(0,2,&self,rhs), m(0,3,&self,rhs);
+                m(1,0,&self,rhs), m(1,1,&self,rhs), m(1,2,&self,rhs), m(1,3,&self,rhs);
+                m(2,0,&self,rhs), m(2,1,&self,rhs), m(2,2,&self,rhs), m(2,3,&self,rhs);
+                m(3,0,&self,rhs), m(3,1,&self,rhs), m(3,2,&self,rhs), m(3,3,&self,rhs)]
     }
 }
