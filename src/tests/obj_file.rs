79dfc7d3694e54072b16a923b8754ac411d14530
klomeli <klomeli@gmail.com>
2022-02-02 17:14:48 -0500

    Parser now detects skipped lines.

--- src/tests/obj_file.rs
+++ src/tests/obj_file.rs
@@ -1,13 +1,12 @@
 #[test]
 fn ch15_test1_ignore_unrecognized_lines() {
-    let contents = r#"
-        There was a young lady named Bright
+    let contents = r#"There was a young lady named Bright
         who traveled much faster than light.
         She set out one day
         in a relative way,
-        and came back the previous night.
-        "#;
+        and came back the previous night."#;
 
     let objs = crate::utils::obj_parser::parse_obj(contents);
-    assert!(objs.unwrap().is_empty());
+    assert!(objs.is_ok());
+    assert_eq!(objs.unwrap().skipped, 5);
 }
--- src/utils/obj_parser.rs
+++ src/utils/obj_parser.rs
@@ -1,7 +1,27 @@
 use crate::domain::object::Object;
 use nom::error::Error;
+use nom::Err;
+use std::io::{BufRead, BufReader};
+
+pub struct ObjParseResult<'a> {
+    pub objects: Vec<Object<'a>>,
+    pub skipped: usize,
+}
 
 // Given obj-formatted content, returns Vec of corresponding Objects
-pub fn parse_obj<'a>(contents: &str) -> Result<Vec<Object<'a>>, Error<&str>> {
-    Result::Ok(vec![])
+pub fn parse_obj<'a>(contents: &str) -> Result<ObjParseResult<'a>, Error<&str>> {
+    let reader = BufReader::new(contents.as_bytes());
+
+    let mut to_return = ObjParseResult {
+        objects: Vec::new(),
+        skipped: 0,
+    };
+
+    for line in reader.lines() {
+        // Result<Vec<Object<'a>>, Error<&str>>
+
+        to_return.skipped += 1;
+    }
+
+    Result::Ok(to_return)
 }
