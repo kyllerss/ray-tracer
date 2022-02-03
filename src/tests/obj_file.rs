551a121ede0492c384d0ab0cf7cb5f9cd2eb76ad
klomeli <klomeli@gmail.com>
2022-02-03 11:08:15 -0500

    Added logic for parsing verteces.

--- src/tests/obj_file.rs
+++ src/tests/obj_file.rs
@@ -1,3 +1,5 @@
+use crate::domain::Point;
+
 #[test]
 fn ch15_test1_ignore_unrecognized_lines() {
     let contents = r#"There was a young lady named Bright
@@ -6,7 +8,27 @@ fn ch15_test1_ignore_unrecognized_lines() {
         in a relative way,
         and came back the previous night."#;
 
-    let objs = crate::utils::obj_parser::parse_obj(contents);
-    assert!(objs.is_ok());
-    assert_eq!(objs.unwrap().skipped, 5);
+    let r = crate::utils::obj_parser::parse_obj_file(contents);
+    assert!(r.is_some());
+    assert_eq!(r.unwrap().skipped, 5);
+}
+
+#[test]
+fn ch15_test2_vertex_records() {
+    let contents = r#"
+        v -1 1 0
+        v -1.0000 0.5000 0.0000
+        v 1 0 0
+        v 1 1 0
+    "#;
+
+    let r = crate::utils::obj_parser::parse_obj_file(contents);
+    assert!(r.is_some());
+
+    let p = r.unwrap();
+    assert_eq!(p.vertex_count(), 4);
+    assert_eq!(p.vertex(1).unwrap(), &Point::new(-1.0, 1.0, 0.0));
+    assert_eq!(p.vertex(2).unwrap(), &Point::new(-1.0, 0.5, 0.0));
+    assert_eq!(p.vertex(3).unwrap(), &Point::new(1.0, 0.0, 0.0));
+    assert_eq!(p.vertex(4).unwrap(), &Point::new(1.0, 1.0, 0.0));
 }
--- src/utils/obj_parser.rs
+++ src/utils/obj_parser.rs
@@ -1,27 +1,74 @@
 use crate::domain::object::Object;
-use nom::error::Error;
-use nom::Err;
+use crate::domain::Point;
+use nom::bytes::complete::tag;
+use nom::error::{Error, VerboseError};
+use nom::number::complete::{double, float};
+use nom::sequence::tuple;
+use nom::{Err, IResult, Parser};
 use std::io::{BufRead, BufReader};
 
 pub struct ObjParseResult<'a> {
+    vertices: Vec<Point>,
     pub objects: Vec<Object<'a>>,
     pub skipped: usize,
 }
 
+impl<'a> ObjParseResult<'a> {
+    // vertices accessor - 1-based indexing
+    pub fn vertex(&self, index: usize) -> Option<&Point> {
+        self.vertices.get(index - 1)
+    }
+
+    pub fn vertex_count(&self) -> usize {
+        self.vertices.len()
+    }
+}
+
+fn vertex(input: &str) -> IResult<&str, Point> {
+    // remove leading whitespace
+    let (r, _) = nom::character::complete::space0(input)?;
+
+    let (remainder, (_, _, x, _, y, _, z)) = tuple((
+        tag("v"),
+        nom::character::complete::char(' '),
+        nom::number::complete::double,
+        nom::character::complete::char(' '),
+        nom::number::complete::double,
+        nom::character::complete::char(' '),
+        nom::number::complete::double,
+    ))(r)?;
+
+    IResult::Ok((remainder, Point::new(x, y, z)))
+}
+
 // Given obj-formatted content, returns Vec of corresponding Objects
-pub fn parse_obj<'a>(contents: &str) -> Result<ObjParseResult<'a>, Error<&str>> {
+pub fn parse_obj_file<'a>(contents: &str) -> Option<ObjParseResult<'a>> {
     let reader = BufReader::new(contents.as_bytes());
 
     let mut to_return = ObjParseResult {
+        vertices: Vec::new(),
         objects: Vec::new(),
         skipped: 0,
     };
 
     for line in reader.lines() {
-        // Result<Vec<Object<'a>>, Error<&str>>
+        if line.is_err() {
+            return Option::None;
+        }
 
-        to_return.skipped += 1;
+        let l = line.unwrap();
+
+        // vertex parsing
+        let v = vertex(l.as_ref());
+
+        if let IResult::Ok((_, vertex)) = v {
+            to_return.vertices.push(vertex);
+        } else {
+            to_return.skipped += 1;
+        }
+
+        // triangle parsing
     }
 
-    Result::Ok(to_return)
+    Option::Some(to_return)
 }
