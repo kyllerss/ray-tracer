000a6ce75daa1e69e7464577c635cc5a3e43ba37
klomeli <klomeli@gmail.com>
2022-02-06 12:17:50 -0500

    Completed test for embedded groups by validating internal triangles.

--- src/tests/obj_file.rs
+++ src/tests/obj_file.rs
@@ -1,11 +1,28 @@
 use crate::domain::object::{Group, Object, Triangle};
 use crate::domain::Point;
 
-fn extract_triangle<'r, 's: 'r>(obj: &'r Object<'s>) -> &'r Triangle<'s> {
+fn extract_triangle_one_level<'r, 's: 'r>(obj: &'r Object<'s>) -> &'r Triangle<'s> {
     match obj {
         Object::Triangle(triangle) => &triangle,
-        _ => panic!("Expected triangle!"),
+        _ => panic!("Expected Object::Triangle!"),
     }
+}
+
+fn extract_triangle_two_level<'r, 's>(target_group: &'r Object<'s>) -> &'r Triangle<'s> {
+    match target_group {
+        Object::Group(inner_group) => {
+            assert_eq!(inner_group.children.len(), 1);
+            match &inner_group.children[0] {
+                Object::Triangle(t) => t,
+                _ => panic!("Expected Object::Triangle!"),
+            }
+        }
+        _ => panic!("Expected Object::Group!"),
+    }
+}
+
+fn weak_triangle_equality_check<'r, 's>(t1: &'r Triangle<'s>, t2: &'r Triangle<'s>) -> bool {
+    t1.p1 == t2.p1 && t1.p2 == t2.p2 && t1.p3 == t2.p3
 }
 
 #[test]
@@ -60,10 +77,10 @@ fn ch15_test9_parsing_triangle_faces() {
     let g = p.default_group();
 
     assert!(g.get(0).is_some());
-    let t1 = extract_triangle(g.get(0).unwrap());
+    let t1 = extract_triangle_one_level(g.get(0).unwrap());
 
     assert!(g.get(1).is_some());
-    let t2 = extract_triangle(g.get(1).unwrap());
+    let t2 = extract_triangle_one_level(g.get(1).unwrap());
 
     assert_eq!(t1.p1, *p.vertex(1).unwrap());
     assert_eq!(t1.p2, *p.vertex(2).unwrap());
@@ -92,13 +109,13 @@ fn ch15_test10_triangulating_polygons() {
     let g = p.default_group();
 
     assert!(g.get(0).is_some());
-    let t1 = extract_triangle(g.get(0).unwrap());
+    let t1 = extract_triangle_one_level(g.get(0).unwrap());
 
     assert!(g.get(1).is_some());
-    let t2 = extract_triangle(g.get(1).unwrap());
+    let t2 = extract_triangle_one_level(g.get(1).unwrap());
 
     assert!(g.get(2).is_some());
-    let t3 = extract_triangle(g.get(2).unwrap());
+    let t3 = extract_triangle_one_level(g.get(2).unwrap());
 
     assert_eq!(t1.p1, *p.vertex(1).unwrap());
     assert_eq!(t1.p2, *p.vertex(2).unwrap());
@@ -134,11 +151,11 @@ fn ch15_test11_triangles_in_groups() {
 
     assert!(g1.is_some());
     assert!(g1.unwrap().get(0).is_some());
-    let t1 = extract_triangle(g1.unwrap().get(0).unwrap());
+    let t1 = extract_triangle_one_level(g1.unwrap().get(0).unwrap());
 
     assert!(g2.is_some());
     assert!(g2.unwrap().get(0).is_some());
-    let t2 = extract_triangle(g2.unwrap().get(0).unwrap());
+    let t2 = extract_triangle_one_level(g2.unwrap().get(0).unwrap());
 
     assert_eq!(t1.p1, *p.vertex(1).unwrap());
     assert_eq!(t1.p2, *p.vertex(2).unwrap());
@@ -167,4 +184,30 @@ fn ch15_test12_convert_obj_file_to_group() {
 
     let g: Box<Group> = r.unwrap().into();
     assert_eq!(g.children.len(), 2);
+
+    let t1_exp = Triangle::builder(
+        Point::new(-1.0, 1.0, 0.0),
+        Point::new(-1.0, 0.0, 0.0),
+        Point::new(1.0, 0.0, 0.0),
+    )
+    .build();
+
+    let t2_exp = Triangle::builder(
+        Point::new(-1.0, 1.0, 0.0),
+        Point::new(1.0, 0.0, 0.0),
+        Point::new(1.0, 1.0, 0.0),
+    )
+    .build();
+
+    let t_a = extract_triangle_two_level(&g.children[0]);
+    let t_b = extract_triangle_two_level(&g.children[1]);
+
+    // Can't rely on order returned for groups, so allow for alternate ordering.
+    if weak_triangle_equality_check(t_a, &t1_exp) {
+        assert!(weak_triangle_equality_check(t_b, &t2_exp));
+    } else if weak_triangle_equality_check(t_b, &t1_exp) {
+        assert!(weak_triangle_equality_check(t_a, &t2_exp));
+    } else {
+        assert!(false);
+    }
 }
